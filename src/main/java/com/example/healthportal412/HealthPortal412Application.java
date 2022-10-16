b6ec5ff8bae4a2d0873d435b1881f31afd9125ed
Brian Gorbea <bag635@psu.edu>
2022-10-15 20:43:53 -0400

    Testing out healthportal with spring MVC. This is the way.

---
+++ src/main/java/com/example/healthportal412/HealthPortal412Application.java
@@ -0,0 +1,13 @@
+package com.example.healthportal412;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPortal412Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthPortal412Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/healthportal412/HealthPortal412ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthportal412;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPortal412ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
