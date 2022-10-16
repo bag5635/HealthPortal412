4761473f3522eea2929e36dca152c20787e1225a
Brian Gorbea <bag635@psu.edu>
2022-10-15 22:00:02 -0400

    Testing out healthportal with spring MVC. This is the way.

--- src/main/java/com/example/healthportal412/HomeController.java
+++ src/main/java/com/example/healthportal412/HomeController.java
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-        @GetMapping("/")
-        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-            model.addAttribute("name", name);
-            return "Home";
-        }
+    @GetMapping("/")
+    public String Login(){
+        return "Login";
+    }
 
 
 }
